0f12d8a81b5969c98956e3861fc99b94868cc216
Muhammad Arslan <gery_maz_555@hotmail.com>
2023-10-26 14:52:46 +0500

    companion objects

---
+++ src/main/kotlin/CompanionClass.kt
@@ -0,0 +1,25 @@
+fun main(){
+    var cc = CompanionClass()
+    // need an object
+    println("First value is : ${cc.firstValue}")
+    println("Sum is : ${cc.add(4,5)}")
+
+    // no need of an object for companion object
+    println("Second Value is : ${CompanionClass.secondValue}")
+    println("Product is : ${CompanionClass.product(5,10)}")
+}
+
+
+class CompanionClass {
+    var firstValue = 10;
+    fun add(a: Int, b: Int) : Int{
+        return a+b
+    }
+
+    companion object{
+        var secondValue = 20
+        fun product(a: Int, b: Int) : Int{
+            return a*b
+        }
+    }
+}
